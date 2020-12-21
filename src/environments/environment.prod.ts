cddf5961124f7f1607f0e720365144fa30c1f715
kumar <kumarpride@gmail.com>
2020-12-21 22:27:18 +0530

    node modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyASfN_OVXgjuLbKKv6TKul-1S14joHFzww",
-    authDomain: "ionfiretaxi.firebaseapp.com",
-    databaseURL: "https://ionfiretaxi.firebaseio.com",
-    projectId: "ionfiretaxi",
-    storageBucket: "ionfiretaxi.appspot.com",
-    messagingSenderId: "493104185856",
-    appId: "1:493104185856:web:9084d6bf644e78e849cee5"
+    apiKey: "AIzaSyAs_5H9bHfOpdcqIDJszbtYLTSSWnwE9_k",
+    authDomain: "ugoadmin.firebaseapp.com",
+    databaseURL: "https://ugoadmin-default-rtdb.firebaseio.com",
+    projectId: "ugoadmin",
+    storageBucket: "ugoadmin.appspot.com",
+    messagingSenderId: "104802353263",
+    appId: "1:104802353263:web:7a35c95c27e6caa266cd9a",
+    measurementId: "G-9455P9ZRXW"
   },
   adminEmail: 'admin@demo.com' // you need to create new account in firebase auth manually
 };
